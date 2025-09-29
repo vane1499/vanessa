51abffbbd8e7ec571c3706e7f00aff399ef573eb
vane1499 <vanessa.huayta@sistemas.edu.bo>
2025-09-28 22:47:55 -0400

    creando la parte de utils.cpp

---
+++ FRONTEND/src/utils.cpp
@@ -0,0 +1,13 @@
+#include "utils.h"
+
+// Aquí se puede agregar utilidades (validaciones, formateo, helpers UI).
+// Archivo placeholder.
+
+/*Un "placeholder" (o marcador de posición) es un elemento temporal que se usa para ocupar 
+el espacio o sugerir el tipo de contenido que se espera en un lugar determinado, y que será 
+reemplazado por el contenido final en el futuro. Se utiliza en diversos contextos como en 
+diseño web y videojuegos para rellenar provisionalmente un espacio, guiar al usuario en 
+formularios o representar elementos no terminados, facilitando el desarrollo y la 
+visualización del proyecto. 
+
+*/
