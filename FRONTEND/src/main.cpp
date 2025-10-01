7928ca44142a8f6a848e558773e72a6c0c2fa5fe
vane1499 <vanessa.huayta@sistemas.edu.bo>
2025-09-30 22:45:03 -0400

    editando el main.cpp

--- FRONTEND/src/main.cpp
+++ FRONTEND/src/main.cpp
@@ -1,12 +1,7 @@
-#include <QApplication>
 #include "mainwindow.h"
+#include <QApplication>
 
 int main(int argc, char *argv[]) {
-    if (argc > 1 && std::string(argv[1]) == "--console") {
-        runConsole();  // función en console.cpp
-        return 0;
-    }
-
     QApplication app(argc, argv);
     MainWindow w;
     w.show();
